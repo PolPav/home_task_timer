8d65349069012941f4380b46c46a48e0247e9a6c
Oleg <krtlmail@gmail.com>
2017-10-31 17:53:11 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ class App extends Component {
     return (
       <div className='wrap_timer'>
       <Timer/>
-          <table className="wrap_timer_table">
-            <thead>
+        <table className="wrap_timer_table">
+          <thead>
             <tr className="wrap_timer_table_tr">
               <th className="wrap_timer_table_tr_td">task name</th>
               <th className="wrap_timer_table_tr_td">start time</th>
@@ -23,20 +23,20 @@ class App extends Component {
               <th className="wrap_timer_table_tr_td">info</th>
               <th className="wrap_timer_table_tr_td">delete</th>
             </tr>
-            </thead>
-              <tbody>
-                {this.props.tasksStore.map((task, idx) =>
-                  <tr key={idx} className="wrap_timer_table_tr">
-                    <td className="wrap_timer_table_tr_td">{task.name}</td>
-                    <td className="wrap_timer_table_tr_td">{task.startTime.toString()}</td>
-                    <td className="wrap_timer_table_tr_td">{task.endTime.toString()}</td>
-                    <td className="wrap_timer_table_tr_td">{task.spentTime}</td>
-                    <td className="wrap_timer_table_tr_td"><button className="wrap_timer_table_button">Info</button></td>
-                    <td className="wrap_timer_table_tr_td"><button className="wrap_timer_table_button">Delete</button></td>
-                  </tr>
-                )}
-              </tbody>
-          </table>
+          </thead>
+          <tbody>
+            {this.props.tasksStore.map((task, idx) =>
+              <tr key={idx} className="wrap_timer_table_tr">
+                <td className="wrap_timer_table_tr_td">{task.name}</td>
+                <td className="wrap_timer_table_tr_td">{task.startTime.toString()}</td>
+                <td className="wrap_timer_table_tr_td">{task.endTime.toString()}</td>
+                <td className="wrap_timer_table_tr_td">{task.spentTime}</td>
+                <td className="wrap_timer_table_tr_td"><button className="wrap_timer_table_button">Info</button></td>
+                <td className="wrap_timer_table_tr_td"><button className="wrap_timer_table_button">Delete</button></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     );
   }
